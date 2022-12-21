00a03b5ecb0bb355e89444607111a6dcdb1ffae9
clausaji <c.sanchezjimenez@qquest.nl>
2022-12-21 08:49:39 +0100

    I generate the answers by maping

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import {nanoid} from "nanoid"
 import Answer from "./Answer"
 
 export default function Question(props){
@@ -12,38 +11,22 @@ export default function Question(props){
     newQuestion = newQuestion.replace(/&#039;/g, "'")
     newQuestion = newQuestion.replace(/&deg;/g, "°")
 
+    const visualAnswers = props.answers.map(oldAnswer =>
+        <Answer className="option" 
+            id={oldAnswer.id}
+            onClick={props.function} 
+            clicked={oldAnswer.clicked}
+            answerText={oldAnswer.answer} 
+            gameEnded={props.gameEnded}
+            amICorrect={oldAnswer.correct}
+        />
+    )
+
     return(
         <div className="questionDiv">
             <p className="question">{newQuestion}</p>
             <div className="optionDiv">
-                <Answer className="option" 
-                    id={props.answers[0].id}
-                    onClick={props.function} 
-                    clicked={props.answers[0].clicked}
-                    answerText={props.answers[0].answer} 
-                    gameEnded={props.gameEnded}
-                    amICorrect={props.answers[0].correct}/>
-                <Answer className="option" 
-                    id={props.answers[1].id}
-                    onClick={props.function} 
-                    clicked={props.answers[1].clicked}
-                    answerText={props.answers[1].answer} 
-                    gameEnded={props.gameEnded}
-                    amICorrect={props.answers[1].correct} />
-                {props.type != "boolean" && <Answer className="option" 
-                    id={props.answers[2].id}
-                    onClick={props.function} 
-                    clicked={props.answers[2].clicked}
-                    answerText={props.answers[2].answer}
-                    gameEnded={props.gameEnded} 
-                    amICorrect={props.answers[2].correct}/>}
-                {props.type != "boolean" && <Answer className="option" 
-                    id={props.answers[3].id}
-                    onClick={props.function} 
-                    clicked={props.answers[3].clicked}
-                    answerText={props.answers[3].answer}
-                    gameEnded={props.gameEnded}
-                    amICorrect={props.answers[3].correct}/>}
+                {visualAnswers}
             </div>
             <hr/>
         </div>
